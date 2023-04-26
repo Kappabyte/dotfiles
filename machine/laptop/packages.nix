1968ea7ebd3e8868616796c51b5e214990855184
Avery Keuben <avery1516@gmail.com>
2023-04-26 15:56:54 -0600

    Remove desktop specific packages from laptop

--- machine/laptop/packages.nix
+++ machine/laptop/packages.nix
@@ -2,10 +2,5 @@
     
     # System packages common to all machines
     environment.systemPackages = with pkgs; [
-        steam
-        libsForQt5.kdenlive
-        prismlauncher
-        glxinfo
-        pciutils
     ];
 }

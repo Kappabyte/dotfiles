8f8ef35d74bc175db7b72543a625d58041864393
Avery Keuben <avery1516@gmail.com>
2023-05-28 20:17:01 -0600

    Update syncthing config

--- nixos/packages/syncthing.nix
+++ nixos/packages/syncthing.nix
@@ -1,3 +1,7 @@
 { config, pkgs, ...}: {
-    services.syncthing.enable = true;
+    services.syncthing = {
+        enable = true;
+        dataDir = "/home/avery";
+        configDir = "/home/avery/.config/syncthing";
+    };
 }

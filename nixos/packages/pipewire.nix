de4a6e61aa144f2440b6fee4052b62a7c2e77ee8
Avery Keuben <avery1516@gmail.com>
2023-08-08 19:20:12 -0600

    Fix audio

--- nixos/packages/pipewire.nix
+++ nixos/packages/pipewire.nix
@@ -2,7 +2,7 @@
     services.pipewire = {
         enable = true;
         alsa.enable = true;
-        alsa.support32Bit = true;
+#        alsa.support32Bit = true;
         pulse.enable = true;
         # If you want to use JACK applications, uncomment this
         jack.enable = true;

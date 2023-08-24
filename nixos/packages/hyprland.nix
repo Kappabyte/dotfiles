2e217beba173bd96c4c1a6e0527af273f8c60171
Avery Keuben <avery1516@gmail.com>
2023-08-24 01:27:26 -0600

    Fix hyprland

--- nixos/packages/hyprland.nix
+++ nixos/packages/hyprland.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
     programs.hyprland = {
         enable = true;
-        nvidiaPatches = true;
+        enableNvidiaPatches = true;
         # xwayland.enable = false;
     };
 }

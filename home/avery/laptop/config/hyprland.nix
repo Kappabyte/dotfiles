f9f8803ee60c4c6e82e13e7af5c5b64616481ab7
Avery Keuben <avery1516@gmail.com>
2023-08-30 22:13:41 -0600

    Screen off when lid closed

--- home/avery/laptop/config/hyprland.nix
+++ home/avery/laptop/config/hyprland.nix
@@ -4,6 +4,11 @@ monitor=eDP-1,2736x1824,0x0,2
 
 exec-once=iio-hyprland
 exec-once=/home/avery/.config/hypr/rotate.sh
+
+bindl=,switch:on:Lid Switch,exec,/home/avery/.config/hypr/lock.sh
+bindl=,switch:on:Lid Switch,exec,brightnessctl -s
+bindl=,switch:on:Lid Switch,exec,brightnessctl s 0%
+bindl=,switch:off:Lid Switch,exec,brightnessctl -r
 '';
 in {
     imports = [

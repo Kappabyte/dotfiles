5a72e0120c5d4cba003f7e82387116c24e927c70
Avery Keuben <avery1516@gmail.com>
2023-09-13 18:52:15 -0600

    Move obsidianmd to common

--- machine/laptop/packages.nix
+++ machine/laptop/packages.nix
@@ -5,7 +5,6 @@
     # System packages common to all machines
     environment.systemPackages = with pkgs; [
         iptsd
-        obsidian
 
         (pkgs.callPackage ../../custom/packages/iio-hyprland/default.nix {}) 
 

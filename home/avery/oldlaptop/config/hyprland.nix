755bcb075bba55a3f69a8eafa37b31c4b4bcbd74
Avery Keuben <avery1516@gmail.com>
2024-02-15 00:35:35 +0000

    add oldlaptop

---
+++ home/avery/oldlaptop/config/hyprland.nix
@@ -0,0 +1,29 @@
+{ lib, pkgs, inputs, ... }: {
+    wayland.windowManager.hyprland = {
+        plugins = [
+            inputs.hyprgrass.packages.${pkgs.system}.default
+        ];
+        settings = {
+            monitor = [
+                "eDP-1,1920x1080,0x0,1,vrr,1"
+            ];
+            bindl = [
+                ",switch:on:Lid Switch,exec,/home/avery/.config/hypr/lock.sh"
+                ",switch:on:Lid Switch,exec,${pkgs.brightnessctl}/bin/brightnessctl -s"
+                ",switch:on:Lid Switch,exec,${pkgs.brightnessctl}/bin/brightnessctl s 0%"
+                ",switch:off:Lid Switch,exec,${pkgs.brightnessctl}/bin/brightnessctl -r"
+            ];
+            bind = [
+                ",edge:u:d,hycov:toggleoverview"
+            ];
+            plugin = {
+                touch_gestures = {
+                    sensitivity = 4.0;
+                    workspace_swipe_fingers = 3;
+                    long_press_delay = 400;
+                };
+            };
+        };
+    };
+
+}
---
+++ system/host/oldlaptop/default.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, hostName, ... }: {
+    imports = [
+        ../../modules
+    ] ++ lib.optional (builtins.pathExists ./packages.nix) ./packages.nix
+      ++ lib.optional (builtins.pathExists ./hardware) ./hardware;
+
+    module.base.enable = true;
+    module.desktop.enable = true;
+    module.development.enable = true;
+    module.tools.enable = true;
+
+    ## Set the hostname
+    networking.hostName = hostName;
+}

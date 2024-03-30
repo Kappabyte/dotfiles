f6ac5c52de33588d7693f83a310adb7a7fe478ce
Avery Keuben <avery1516@gmail.com>
2024-03-29 20:08:00 -0600

    fix nixvim cmp

--- home/avery/base/config/nvim.nix
+++ home/avery/base/config/nvim.nix
@@ -1,5 +1,9 @@
 {pkgs, config, ...}: {
 
+
+
+
+
     home.sessionVariables = {
         EDITOR = "nvim";
     };
@@ -99,14 +103,24 @@
             };
             cmp = {
                 enable = true;
-                autoEnableSources = true;
                 settings = {
+                    snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+                    completion.completeopt = "noselect";
+                    preselect = "None";
                     mapping = {
+                        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+                        "<C-f>" = "cmp.mapping.scroll_docs(4)";
                         "<C-Space>" = "cmp.mapping.complete()";
-                        "<Tab>" = "cmp.mapping.confirm({ select = true })";
-                        "<C-q>" = "cmp.mapping(cmp.select_prev_item(), {'i', 's'})";
-                        "<C-e>" = "cmp.mapping(cmp.select_next_item(), {'i', 's'})";
+                        "<C-e>" = "cmp.mapping.abort()";
+                        "<CR>" = "cmp.mapping.confirm({ select = true })";
+                        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+                        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
                     };
+                    sources = [
+                        {name = "nvim_lsp";}
+                        {name = "path";}
+                        {name = "buffer";}
+                    ];
                 };
             };
             neogit = {

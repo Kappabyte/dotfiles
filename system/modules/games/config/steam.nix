b73ed24d0f0a00d628f18a06f2a698a5cf1012a8
Avery Keuben <avery1516@gmail.com>
2024-06-20 16:45:03 -0600

    Add proton-ge

--- system/modules/games/config/steam.nix
+++ system/modules/games/config/steam.nix
@@ -3,6 +3,9 @@
         enable = true;
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        extraCompatPackages = with pkgs; [
+            proton-ge-bin
+        ];
     };
     programs.gamescope = {
         enable = true;
